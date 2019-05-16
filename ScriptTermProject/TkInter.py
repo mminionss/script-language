c57db6a7f737649668353644b79a92121c917366
MINIONS <tiurit12@naver.com>
2019-05-17 00:00:20 +0900

    tkInter, 지도
    대략적인 tkInter 구성완료
    지도 옵션 수정

--- ScriptTermProject/TkInter.py
+++ ScriptTermProject/TkInter.py
@@ -1,24 +1,27 @@
 import tkinter
 from tkinter import*
+from tkinter import Tk, Label, PhotoImage
+
 
 class Animals:
     def __init__(self):
-        window = Tk()
+        window = tkinter.Tk()
         window.title("유기 동물 조회 서비스")
+        window.resizable(False, False)
 
         frame1 = Frame(window)
         frame1.pack()
         self.v=IntVar()
         Label(frame1, text="검색옵션").grid(row=0, column=0,sticky="W")
         Radiobutton(frame1, text = "강아지", variable=self.v, value = 1).grid(row=1, column=0,sticky="W")
-        Radiobutton(frame1, text = "고양이", variable = self.v, value = 1).grid(row=1, column=1,sticky="W")
-        Radiobutton(frame1, text="전체", variable=self.v, value=1).grid(row=1, column=2,sticky="W")
+        Radiobutton(frame1, text = "고양이", variable = self.v, value = 2).grid(row=1, column=1,sticky="W")
+        Radiobutton(frame1, text="전체", variable=self.v, value=3).grid(row=1, column=2,sticky="W")
 
         OPTIONS = [
-            'Jan',
-            'Feb',
-            'March',
-            'April'
+            '전체',
+            '서울',
+            '인천',
+            '경기'
         ]  # etc
 
         variable = StringVar(window)
@@ -41,7 +44,38 @@ class Animals:
         b3 = Button(frame3, text="지역순")
         b3.grid(row=0, column=2, sticky="E")
 
+        frame7 = Frame(window)
+        frame7.pack()
+        scrollbar = tkinter.Scrollbar(frame7)
+        scrollbar.pack(side="right", fill="y")
+        listbox = tkinter.Listbox(frame7, yscrollcommand=scrollbar.set)
+
+        listbox.pack(side="left")
+        scrollbar["command"] = listbox.yview
+
+        frame4 = Frame(window)
+        frame4.pack()
+        b4 = Button(frame4, text="이메일")
+        b4.grid(row=0, column=1, sticky="E")
+        b5 = Button(frame4, text="☆")
+        b5.grid(row=0, column=5, sticky="E")
+
+        frame5 = Frame(window)
+        frame5.pack()
+        b6 = Button(frame5, text="보호소 위치보기")
+        b6.grid(row=0, column=3, sticky="E")
+
+        imgObj = PhotoImage(file="ex.gif")
+
+        imgLabel = Label(window)
+        imgLabel.config(image=imgObj)
+        imgLabel.pack()
+
+
         window.mainloop()
 
 
+
+
 Animals()
+
--- ScriptTermProject/foliumTest.py
+++ ScriptTermProject/foliumTest.py
@@ -4,10 +4,10 @@ import folium
 import webbrowser
 
 # 위도 경도 지정
-map_osm = folium.Map (location = [37.568477, 126.981611],zoom_start=13)
+map_osm = folium.Map (location = [37.568477, 126.981611],zoom_start=16)
 # 마커 지정
-folium.Marker([37.568477, 126.981611], popup='Mt. Hood Meadows').add_to(map_osm)
+#folium.Marker([37.568477, 126.981611], popup='Mt. Hood Meadows').add_to(map_osm)
 # html 파일로 저장
 map_osm.save('osm.html')
 
-webbrowser.open("C:\\Users\\tiuri\\Documents\\GitHub\\script-language\\ScriptTermProject\\osm.html")
+webbrowser.open("osm.html")
